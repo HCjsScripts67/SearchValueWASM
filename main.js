67836bbedf307768bbb99832e17a27b78f4228a5
HCjsScripts67 <gmang6101@gmail.com>
2026-02-23 14:26:37 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,1 @@
- help() {
-            console.log('📚 ДОСТУПНЫЕ КОМАНДЫ:');
-            console.log('  searchALL(start, end) - сканировать память');
-            console.log('  showResults() - показать результаты');
-            console.log('  myHack.results - массив результатов');
-        },
 console.log('✅ main.js успешно загружен!');
-window.scriptLoaded = true;
