2dfa29962fecc2d5659e98111a07d357db14bce8
HCjsScripts67 <gmang6101@gmail.com>
2026-02-23 14:13:35 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+ help() {
+            console.log('📚 ДОСТУПНЫЕ КОМАНДЫ:');
+            console.log('  searchALL(start, end) - сканировать память');
+            console.log('  showResults() - показать результаты');
+            console.log('  myHack.results - массив результатов');
+        },
